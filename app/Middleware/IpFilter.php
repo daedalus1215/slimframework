3be47cd705f44a2699da842ced3ca94f5cc31308
larry adams <ladams1776@yahoo.com>
2017-02-21 21:34:52 -0500

    added global middleware, so that it applies to all routes. To see  functionality, create ip address entry in the table with 127.0.0.1

---
+++ app/Middleware/IpFilter.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ * Filter class that will be used to ban ip addresses.
+ * Created by PhpStorm.
+ * User: ladams
+ * Date: 2/21/2017
+ * Time: 9:18 PM
+ */
+
+namespace App\Middleware;
+
+use PDO;
+
+class IpFilter
+{
+    /**
+     * @var \PDO
+     */
+    protected $db;
+
+    /**
+     * IpFilter constructor.
+     * @param \PDO $db
+     */
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function __invoke($request, $response, $next)
+    {
+        if(!$this->allowed($_SERVER['REMOTE_ADDR'])) {
+            return $response->withStatus(401)->write('Denied');
+        }
+
+        return $next($request, $response);
+    }
+
+
+    protected function allowed($ip)
+    {
+        $ips = $this->db->query("SELECT ip FROM blocked")->fetchAll(PDO::FETCH_COLUMN, 0);
+
+        return !in_array($ip, $ips);
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,15 @@
 <?php
 
-use App\Controllers\ExampleController;
-use App\Controllers\TopicController;
-use App\Middleware\UnAuthenticatedRedirect;
+use App\Middleware\IpFilter;
 
-// Different ways of handling routes.
 
-// 1. Anonymous route handler
+// Adding global middleware.
+$app->add(new IpFilter($container['db']));
+
+$app->get('/', function () {
+    return 'Home';
+});
+
 $app->get('/login', function () {
-    return 'login';
-})->setName('login');
-
-
-// 2. Named route handler
-//$app->get('/login', $loginHandler);
-
-
-
-
-
-
-/**
- * Simulate CSRF
- *
- *
- * @param \Slim\Http\Request $request
- * @param $response
- * @param $next
- * @return mixed
- */
-$tokenMiddleware = function ($request, $response, $next) {
-    $request = $request->withAttribute('token', 'abc123');
-    return $next($request, $response);
-};
-
-$app->group('', function () {
-    // 3. Route Controller
-    $this->get('/topics',  TopicController::class . ':index');
-
-    // 4. Route Controller with Middleware
-    $this->get('/topics/create',  TopicController::class . ':show');
-
-})->add(new UnAuthenticatedRedirect($container))->add($tokenMiddleware);
-
-
+   return 'Login';
+});
